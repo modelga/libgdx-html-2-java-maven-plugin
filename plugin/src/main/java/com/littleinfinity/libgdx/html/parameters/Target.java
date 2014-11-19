d740d8f6fb1faf4bf28dc656770cf9e5fb69c548
Mateusz Odelga <m.odelga@vailantu.net>
2014-11-19 22:14:37 +0100

    [GDXHTML-4] Add GenerateMojo and simple configuration structures.

---
+++ plugin/src/main/java/com/littleinfinity/libgdx/html/parameters/Target.java
@@ -0,0 +1,25 @@
+package com.littleinfinity.libgdx.html.parameters;
+
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+
+public class Target {
+    @Parameter(required = true)
+    private File dir;
+    @Parameter(defaultValue = "")
+    private String packageName;
+    private boolean overwrite;
+
+    public File getDir() {
+        return dir;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public boolean isOverwrite() {
+        return overwrite;
+    }
+}
