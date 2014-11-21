fb2501bbb017a24674df3d00dbfe05c3d7120c20
Mateusz Odelga <m.odelga@vailantu.net>
2014-11-21 08:30:03 +0100

    [GDXHTML-6] Add annotations to replace isAbleToParse method

---
+++ plugin/src/main/java/com/littleinfinity/libgdx/html/parser/ParsableTags.java
@@ -0,0 +1,10 @@
+package com.littleinfinity.libgdx.html.parser;
+
+import java.lang.annotation.*;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface ParsableTags {
+    String[] tags();
+}
