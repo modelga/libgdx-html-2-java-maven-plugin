e75b202ef17b41c290d964f4a14e87181ae9156f
Mateusz Odelga <modelga@future-processing.com>
2014-12-03 13:53:13 +0100

    [GDXHTML-6] Add ability to walking through file tree from input source.  Add sample file to avoid throwing FileNotFound exception on execution project.

--- plugin/src/main/java/com/littleinfinity/libgdx/html/JavaClassBootstraper.java
+++ plugin/src/main/java/com/littleinfinity/libgdx/html/JavaClassBootstraper.java
@@ -2,15 +2,28 @@ package com.littleinfinity.libgdx.html;
 
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.littleinfinity.libgdx.html.generator.Generator;
+import com.littleinfinity.libgdx.html.generator.java.JavaClassGenerator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.InputMismatchException;
 
 public class JavaClassBootstraper extends Bootstraper {
+    private final Generator classGenerator;
+
     @Inject
     public JavaClassBootstraper(Injector injector, Config config) {
         super(injector, config);
+        classGenerator = injector.getInstance(JavaClassGenerator.class);
     }
 
     @Override
     public void bootstrap() {
-
+        try {
+            Files.walkFileTree(config.getSourceDir().toPath(), new HtmlFileVisitor(classGenerator));
+        } catch (IOException e) {
+            throw new InputMismatchException(e.getMessage());
+        }
     }
 }
