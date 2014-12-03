6c4a638e417f37c89a0d2396b44002bcc583d8d4
Mateusz Odelga <modelga@future-processing.com>
2014-12-03 13:44:10 +0100

    [GDXHTML-6] Update JavaClassGenerator and it's interface to return JavaClassSource from passed file

--- plugin/src/main/java/com/littleinfinity/libgdx/html/generator/java/JavaClassGenerator.java
+++ plugin/src/main/java/com/littleinfinity/libgdx/html/generator/java/JavaClassGenerator.java
@@ -1,34 +1,29 @@
 package com.littleinfinity.libgdx.html.generator.java;
 
-import com.google.inject.Injector;
 import com.google.inject.Singleton;
-import com.littleinfinity.libgdx.html.Config;
 import com.littleinfinity.libgdx.html.generator.Generator;
-import com.littleinfinity.libgdx.html.parser.HTMLTagParser;
-import org.apache.commons.lang3.NotImplementedException;
+import com.littleinfinity.libgdx.html.parser.head.HeadParseFactory;
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 
 import javax.inject.Inject;
 import java.io.File;
-import java.util.Set;
 
 @Singleton
 public class JavaClassGenerator implements Generator {
 
-    private Set<HTMLTagParser<Object>> parsers;
-    private Config config;
-    private Injector injector;
+    private BinderFactory binderFactory;
+    private HeadParseFactory parseFactory;
 
     @Inject
-    public JavaClassGenerator(Set<HTMLTagParser<Object>> parsers, Config config) {
-        this.parsers = parsers;
-        this.config = config;
+    public JavaClassGenerator(BinderFactory binderFactory, HeadParseFactory parseFactory) {
+        this.binderFactory = binderFactory;
+        this.parseFactory = parseFactory;
     }
 
     @Override
-    public void generate(File f) {
+    public JavaClassSource generate(File f) {
         JavaClassSource source = Roaster.create(JavaClassSource.class);
-        throw new NotImplementedException("");
+        return source;
     }
 }
