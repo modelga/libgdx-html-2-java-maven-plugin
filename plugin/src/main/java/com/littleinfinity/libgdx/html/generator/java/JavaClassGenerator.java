8db8ca86a2716eccfd67bad60dc71e5e421defbc
Mateusz Odelga <m.odelga@vailantu.net>
2014-12-04 09:34:15 +0100

    [GDXHTML-8] Support JavaClassGenerator to parse whole Document

--- plugin/src/main/java/com/littleinfinity/libgdx/html/generator/java/JavaClassGenerator.java
+++ plugin/src/main/java/com/littleinfinity/libgdx/html/generator/java/JavaClassGenerator.java
@@ -2,28 +2,44 @@ package com.littleinfinity.libgdx.html.generator.java;
 
 import com.google.inject.Singleton;
 import com.littleinfinity.libgdx.html.generator.Generator;
-import com.littleinfinity.libgdx.html.parser.ParserFactory;
+import com.littleinfinity.libgdx.html.parser.DocumentParser;
+import org.apache.commons.io.IOUtils;
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 
 @Singleton
 public class JavaClassGenerator implements Generator {
 
     private BinderFactory binderFactory;
-    private ParserFactory parseFactory;
+    private DocumentParser documentParser;
 
     @Inject
-    public JavaClassGenerator(BinderFactory binderFactory, ParserFactory parseFactory) {
+    public JavaClassGenerator(BinderFactory binderFactory, DocumentParser documentParser) {
         this.binderFactory = binderFactory;
-        this.parseFactory = parseFactory;
+        this.documentParser = documentParser;
     }
 
     @Override
     public JavaClassSource generate(File f) {
         JavaClassSource source = Roaster.create(JavaClassSource.class);
+        Document document = tryParseDocument(f);
+        ComplexComponent component = documentParser.parse(document);
         return source;
+    }
+
+    private Document tryParseDocument(File f) {
+        Parser parser = Parser.xmlParser();
+        try {
+            return parser.parseInput(IOUtils.toString(f.toURI()), "");
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
