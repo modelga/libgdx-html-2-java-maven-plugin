dd059c7d7e105e6b5fdbf05f5dca9a1c1683f45d
Mateusz Odelga <m.odelga@vailantu.net>
2014-12-04 12:44:21 +0100

    [GDXHTML-8] Support head parsing by delegating childs to proper parser

--- plugin/src/main/java/com/littleinfinity/libgdx/html/parser/head/HeadParser.java
+++ plugin/src/main/java/com/littleinfinity/libgdx/html/parser/head/HeadParser.java
@@ -1,6 +1,7 @@
 package com.littleinfinity.libgdx.html.parser.head;
 
 import com.google.inject.Singleton;
+import com.littleinfinity.libgdx.html.generator.java.ComplexComponent;
 import com.littleinfinity.libgdx.html.generator.java.ImportComponent;
 import com.littleinfinity.libgdx.html.generator.java.JavaSourceComponent;
 import com.littleinfinity.libgdx.html.parser.AnnotatedHTMLTagParser;
@@ -10,16 +11,27 @@ import com.littleinfinity.libgdx.html.parser.ParserFactory;
 import com.littleinfinity.libgdx.html.util.FactoryDependant;
 import org.jsoup.nodes.Element;
 
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
 @Singleton
 @ParsableTags(tags = {"head"})
-public class HeadParser extends AnnotatedHTMLTagParser<JavaSourceComponent> implements FactoryDependant<ParserFactory> {
+public class HeadParser extends AnnotatedHTMLTagParser<ComplexComponent> implements FactoryDependant<ParserFactory> {
 
     private ParserFactory factory;
 
     @Override
-    public JavaSourceComponent parse(Element element) {
-        HTMLTagParser<ImportComponent> parser = factory.getProperParser("");
-        return null;
+    public ComplexComponent parse(Element element) {
+        ComplexComponent complexComponent = new ComplexComponent();
+        HTMLTagParser<ImportComponent> importParser = factory.getProperParser("import");
+        for (Element child : element.children()) {
+            Optional<HTMLTagParser<JavaSourceComponent>> parser = ofNullable(factory.getProperParser(child.tagName()));
+            if (parser.isPresent()) {
+                complexComponent.addComponent(parser.get().parse(child));
+            }
+        }
+        return complexComponent;
     }
 
     @Override
