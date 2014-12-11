0b6688e6fc87cf5de96d15a134bc903313ebd728
Mateusz Odelga <m.odelga@vailantu.net>
2014-12-11 21:58:49 +0100

    Prepare HeadParserTest for deep testing

--- plugin/src/test/java/com/littleinfinity/libgdx/html/parser/head/HeadParserTest.java
+++ plugin/src/test/java/com/littleinfinity/libgdx/html/parser/head/HeadParserTest.java
@@ -1,11 +1,29 @@
 package com.littleinfinity.libgdx.html.parser.head;
 
+import com.littleinfinity.libgdx.html.generator.java.ComplexComponent;
+import com.littleinfinity.libgdx.html.generator.java.JavaSourceComponent;
+import com.littleinfinity.libgdx.html.parser.AnnotatedHTMLTagParser;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 public class HeadParserTest {
+
+    class InsideParser extends AnnotatedHTMLTagParser<InsideComponent> {
+
+        @Override
+        public InsideComponent parse(Element element) {
+            return null;
+        }
+    }
+
+    class InsideComponent extends JavaSourceComponent {
+
+    }
+
     @Test
     public void shouldCreateInstance() {
         HeadParser parser = new HeadParser();
@@ -23,5 +41,11 @@ public class HeadParserTest {
 
         // then
         assertThat(parsed).isNotNull();
+    }
+
+    @Test
+    @Ignore
+    public void shouldParseInsideElement() {
+
     }
 }
