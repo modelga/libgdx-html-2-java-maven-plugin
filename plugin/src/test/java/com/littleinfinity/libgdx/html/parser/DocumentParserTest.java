2c3b0e639b53077bf38fc79c87fa09d1ecffd2a3
Mateusz Odelga <modelga@future-processing.com>
2014-12-12 13:25:15 +0100

    Test DocumentParser

---
+++ plugin/src/test/java/com/littleinfinity/libgdx/html/parser/DocumentParserTest.java
@@ -0,0 +1,91 @@
+package com.littleinfinity.libgdx.html.parser;
+
+import com.littleinfinity.libgdx.html.generator.java.ComplexComponent;
+import com.littleinfinity.libgdx.html.generator.java.JavaSourceComponent;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DocumentParserTest {
+
+    private ParserFactory factory;
+    private DocumentParser parser;
+
+    @Before
+    public void setUpParser() {
+        factory = mock(ParserFactory.class);
+        parser = new DocumentParser(factory);
+    }
+
+    @Test
+    public void shouldNotBeAbleToParseWithoutHeadSection() {
+        // given
+        Document document = createDocumentWithElements("body");
+
+        // when
+        boolean ableToParse = parser.isAbleToParse(document);
+
+        // then
+        assertThat(ableToParse).isFalse();
+    }
+
+    @Test
+    @Ignore // TODO need to be implemented
+    public void shouldNotBeAbleToParseWithoutBodySection() {
+        // given
+        Document document = createDocumentWithElements("head");
+
+        // when
+        boolean ableToParse = parser.isAbleToParse(document);
+
+        // then
+        assertThat(ableToParse).isFalse();
+    }
+
+    @Test
+    public void shouldParseWithHeadAndBodySections() {
+        // given
+        Document document = createDocumentWithElements("body", "head");
+
+        // when
+        boolean ableToParse = parser.isAbleToParse(document);
+
+        // then
+        assertThat(ableToParse).isTrue();
+    }
+
+    @Test
+    public void shouldParseDocument() {
+        // given
+        HTMLTagParser<JavaSourceComponent> headParser = mock(HTMLTagParser.class);
+        when(factory.getProperParser("head")).thenReturn(headParser);
+        Document document = createDocumentWithElements("head");
+        ComplexComponent complexComponent = new ComplexComponent();
+        when(headParser.parse(document.head())).thenReturn(complexComponent);
+
+        // when
+        ComplexComponent parse = parser.parse(document);
+
+        // then
+        assertThat(parse.getAll()).hasSize(1);
+        assertThat(parse.getAll().get(0)).isEqualTo(complexComponent);
+        verify(headParser, times(1)).parse(document.head());
+    }
+
+    private Document createDocumentWithElements(String... elements) {
+        Document document = new Document("");
+        for (String element : elements) {
+            document.appendChild(new Element(Tag.valueOf(element), ""));
+        }
+        return document;
+    }
+}
