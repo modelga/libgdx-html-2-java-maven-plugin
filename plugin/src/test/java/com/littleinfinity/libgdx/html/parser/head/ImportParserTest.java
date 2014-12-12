7d20197d57f3887b91b020596ead1fd998826529
Mateusz Odelga <modelga@future-processing.com>
2014-12-12 10:11:43 +0100

    Test ImportParser

---
+++ plugin/src/test/java/com/littleinfinity/libgdx/html/parser/head/ImportParserTest.java
@@ -0,0 +1,29 @@
+package com.littleinfinity.libgdx.html.parser.head;
+
+import com.littleinfinity.libgdx.html.generator.java.ImportComponent;
+import org.fest.assertions.Assertions;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
+import org.junit.Test;
+
+public class ImportParserTest {
+
+    @Test
+    public void shouldParseImport() throws Exception {
+        // given
+        ImportParser parser = new ImportParser();
+        Element element = createImportElement("org.element.Element");
+
+        // when
+        ImportComponent component = parser.parse(element);
+
+        // then
+        Assertions.assertThat(component.getName()).isEqualTo("org.element.Element");
+    }
+
+    private Element createImportElement(String importValue) {
+        Element element = new Element(Tag.valueOf("import"), "");
+        element.attr("name", importValue);
+        return element;
+    }
+}
